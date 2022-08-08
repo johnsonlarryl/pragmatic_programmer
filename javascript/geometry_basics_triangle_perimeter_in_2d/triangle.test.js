f8db20e080b777125c9a7f2fa14fe7e347c9ee20
Larry Johnson <johnson.larry.l@gmail.com>
2022-08-08 10:37:37 -0500

    Update triangle.test.js

--- javascript/geometry_basics_triangle_perimeter_in_2d/triangle.test.js
+++ javascript/geometry_basics_triangle_perimeter_in_2d/triangle.test.js
@@ -5,7 +5,7 @@ test('adds sides a + b + c equal 120', () => {
     let a = new Point(10, 10);
     let b = new Point(40, 10);
     let c = new Point(10, 50);
-    let triangle = new Triangle(a, b,c );
+    let triangle = new Triangle(a, b, c );
     let actual_perimeter = triangle.perimeter();
 
     expect(actual_perimeter).toBe(expect_perimeter);
@@ -16,7 +16,7 @@ test('adds sides a + b + c equal 135.314734', () => {
     let a = new Point(15, -10);
     let b = new Point(40, 20);
     let c = new Point(20, 50);
-    let triangle = new Triangle(a, b,c );
+    let triangle = new Triangle(a, b, c );
     let actual_perimeter = triangle.perimeter();
 
     expect(actual_perimeter).toBe(expect_perimeter);
